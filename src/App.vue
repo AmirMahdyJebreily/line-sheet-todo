249d683ffce65c299e94c5db1c7064ec54f83f99
AmirMahdyJebreily <amirmahdyjdx@gmail.com>
2023-05-27 03:45:32 +0330

    Add fonts

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
   <header>
- 
+    <p>سلام خدا بر تو</p>
   </header>
 
   <RouterView />
 </template>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Vazirmatn:wght@200;300;700;800;900&display=swap');
+</style>
