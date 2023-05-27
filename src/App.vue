d4070a79ee9e3c20ba7e33a495ffdf8ad5f3866f
AmirMahdyJebreily <amirmahdyjdx@gmail.com>
2023-05-27 03:48:56 +0330

    Add fonts and configs

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,3 @@
 
   <RouterView />
 </template>
-
-<style>
-  @import url('https://fonts.googleapis.com/css2?family=Vazirmatn:wght@200;300;700;800;900&display=swap');
-</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,14 @@ module.exports = {
   ],
   theme: {
     extend: {
-
+            fontFamily: {
+                'vazir': ['Vazirmatn-Regular', 'Calibri', 'Arial Narrow',],
+                'vazir-black': ['Vazirmatn-Black', 'Calibri', 'Arial Narrow',],
+                'vazir-bold': ['Vazirmatn-Bold', 'Calibri', 'Arial Narrow',],
+                'vazir-medium': ['Vazirmatn-Medium', 'Calibri', 'Arial Narrow',],
+                'vazir-light': ['Vazirmatn-Light', 'Calibri', 'Arial Narrow',],
+                'vazir-thin': ['Vazirmatn-Thin', 'Calibri', 'Arial Narrow',],
+            },
     },
   },
   plugins: [],
