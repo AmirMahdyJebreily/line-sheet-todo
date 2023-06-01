e1bd39a1b5dd8873e5b17b89ef721b09e2341cd3
AmirMahdyJebreily <amirmahdyjdx@gmail.com>
2023-06-01 23:02:14 +0330

    Add components

---
+++ src/components/Design/MacButtons.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+    <div class="flex justify-end items-center gap-2 sm:w-full">
+        <span class="w-3 h-3 opacity-75 rounded-full bg-red-500"></span>
+        <span class="w-3 h-3 opacity-75 rounded-full bg-yellow-500"></span>
+        <span class="w-3 h-3 opacity-75 rounded-full bg-green-500"></span>
+    </div>
+</template>
